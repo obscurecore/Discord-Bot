c089583f6d679a07b61dbb8f6f81093220dffe98
Potapov Ruslan <pruslang@gmail.com>
2020-05-17 02:13:29 +0300

    fix serialize and deserialize setting for bot with NIO approach

--- Bot/src/main/java/ru/ruslan/CEWBot.java
+++ Bot/src/main/java/ru/ruslan/CEWBot.java
@@ -1,15 +1,10 @@
 package ru.ruslan;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.lang.reflect.Type;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,62 +18,107 @@ import com.google.gson.reflect.TypeToken;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventDispatcher;
+import sx.blah.discord.handle.impl.obj.User;
 
 public class CEWBot {
+    public static final Logger LOGGER = LoggerFactory.getLogger(CEWBot.class);
+    public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    public static final File JARPATH = new File(".").getAbsoluteFile();
+    public static Config config;
+    public static Map<Long, Collection<Channel>> channels;
+    public static IDiscordClient client;
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(CEWBot.class);
-	public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-	public static final File JARPATH = new File(".").getAbsoluteFile();
-	public static Config config;
-	public static Map<Long, Collection<Channel>> channels;
-	public static IDiscordClient client;
+    public static void main(final String[] args) throws ConfigException {
 
-	public static void main(final String[] args) throws ConfigException {
-		saveConfigs();
-		client = createClient("NzA5NzA5ODE2NDk4NDg3MzQ4.Xr8DFw.xBT6tdMiMXiWQmeQHv-uV1YvCO0", true);
-		final EventDispatcher dispatcher = client.getDispatcher();
-		dispatcher.registerListener(new EventListener());
-		LOGGER.info("Running CEWBot");
-	}
+        loadConfigs();
+        client = createClient("NzA5NzA5ODE2NDk4NDg3MzQ4.Xr_bcg.tvnKrAiQilJDwt8CKTE--ZVBxn0", true);
 
-	public static IDiscordClient createClient(final String token, final boolean login) {
-		final ClientBuilder clientBuilder = new ClientBuilder();
-		clientBuilder.withToken(token);
-		if (login)
-			return clientBuilder.login();
-		else
-			return clientBuilder.build();
-	}
+        /**
+         * The EventDispatcher stores a registry of listeners which are used on every event being dispatched.
+         * Dispatching of these events happens asynchronously
+         * either on user provided threadpools, or a default threadpool.
+         */
 
-	public static void loadConfigs() throws ConfigException {
-		try {
-			final File cfgFile = new File(JARPATH, "config.json");
-			if (!cfgFile.exists())
-				GSON.toJson(Config.getDefault(), new BufferedWriter(new FileWriter(cfgFile)));
-			config = GSON.fromJson(new BufferedReader(new FileReader(cfgFile)), Config.class);
+        final EventDispatcher dispatcher = client.getDispatcher();
 
-			final File channelFile = new File(JARPATH, "channels.json");
-			final Type type = new TypeToken<Map<Long, Collection<Long>>>() {
-			}.getType();
-			if (!channelFile.exists())
-				GSON.toJson(new HashMap<Long, Collection<Channel>>(), new BufferedWriter(new FileWriter(channelFile)));
-			channels = GSON.fromJson(new BufferedReader(new FileReader(channelFile)), type);
-		} catch (JsonSyntaxException|JsonIOException|IOException e) {
-			throw new ConfigException("Config load error", e);
-		}
-	}
+        /**
+         * When registering a listener, the client has the option of specifying the thread pool
+         * for that particular listener, this way, different listeners are effectively
+         * isolated in terms of threads, avoiding possible thread starvation.
+         */
 
-	public static void saveConfigs() throws ConfigException {
-		try {
-			final File cfgFile = new File(JARPATH, "config.json");
-			GSON.toJson(Config.getDefault(), new BufferedWriter(new FileWriter(cfgFile)));
+        dispatcher.registerListener(new EventListener());
+        LOGGER.info("Running CEWBot");
+    }
 
-			final File channelFile = new File(JARPATH, "channels.json");
-			final Type type = new TypeToken<Map<Long, Collection<Channel>>>() {
-			}.getType();
-			GSON.toJson(channels, new BufferedWriter(new FileWriter(channelFile)));
-		} catch (JsonIOException|IOException e) {
-			throw new ConfigException("Config save error", e);
-		}
-	}
+    public static IDiscordClient createClient(final String token, final boolean login) {
+        final ClientBuilder clientBuilder = new ClientBuilder();
+        clientBuilder.withToken(token);
+        if (login)
+            return clientBuilder.login();
+        else
+            return clientBuilder.build();
+    }
+
+    public static void loadConfigs() throws ConfigException {
+        Writer writer = null;
+
+        try {
+            final File cfgFile = new File(JARPATH, "config.json");
+            // Cause Writer create file
+            if (!cfgFile.exists()) {
+                writer = Files.newBufferedWriter(cfgFile.toPath());
+                // Serialize
+                GSON.toJson(Config.getDefault(), writer);
+                writer.close();
+            }
+            // Deserialize
+            config = GSON.fromJson(new BufferedReader(new FileReader(cfgFile)), Config.class);
+
+        } catch (JsonSyntaxException | JsonIOException | IOException e) {
+            throw new ConfigException("Config load  error", e);
+        }
+
+        try {
+            final File channelFile = new File(JARPATH, "channels.json");
+            final Type type = new TypeToken<Map<Long, Collection<Long>>>() {
+            }.getType();
+            if (!channelFile.exists()) {
+                writer = Files.newBufferedWriter(channelFile.toPath());
+                GSON.toJson(new HashMap<Long, Collection<Channel>>(), writer);
+                writer.close();
+            }
+
+            channels = GSON.fromJson(new BufferedReader(new FileReader(channelFile)), type);
+        } catch (JsonSyntaxException | JsonIOException | IOException e) {
+            throw new ConfigException("Channel load error", e);
+        }
+    }
+
+    public static void saveConfigs() throws ConfigException {
+        Writer writer = null;
+
+        try {
+            final File cfgFile = new File(JARPATH, "config.json");
+            writer = Files.newBufferedWriter(cfgFile.toPath());
+            GSON.toJson(Config.getDefault(), writer);
+            writer.close();
+
+        } catch (JsonIOException | IOException e) {
+            throw new ConfigException("Config save error", e);
+        }
+
+        try {
+            final File channelFile = new File(JARPATH, "channels.json");
+            writer = Files.newBufferedWriter(channelFile.toPath());
+            final Type type = new TypeToken<Map<Long, Collection<Channel>>>() {
+            }.getType();
+
+            GSON.toJson(channels, writer);
+            writer.close();
+
+        } catch (JsonIOException | IOException e) {
+            throw new ConfigException("Channel save error", e);
+        }
+    }
 }
